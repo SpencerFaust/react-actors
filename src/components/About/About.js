a0ab010c0c619b68fad272be16bea815d8101cd9
Spencer Faust <faus0059@umn.edu>
2019-02-19 17:24:45 -0600

    All files added and up to date.

---
+++ src/components/About/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+    render() {
+        return (
+            <div>
+                <h2>ABout</h2>
+                <p>We are a group of actors who REACT to the world around us!</p>
+            </div>
+        )
+    };
+};
+
+export default About;
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Contact</h2>
+                <p>Phone Number: 555.555.5555</p>
+                <p>Email: Reactors@reactors.com</p>
+            </div>
+        )
+    };
+};
+
+export default Contact;
